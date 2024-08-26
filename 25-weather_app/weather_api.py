640ac9623470b82ac5a343ef2d0659978566e0c7
hamid <86283611+hamidrezaei6@users.noreply.github.com>
2024-08-26 22:25:23 +0330

    Update weather_api.py

--- 25-weather_app/weather_api.py
+++ 25-weather_app/weather_api.py
@@ -5,7 +5,7 @@ from typing import Final
 from model import Weather, dt
 
 # Constants
-API_KEY: Final[str] = '4d18b09dc3cf1463486f02312b9b663b'
+API_KEY: Final[str] = 'YOUR-KEY'
 BASE_URL: Final[str] = 'https://api.openweathermap.org/data/2.5/forecast'
 
 
